469dda02bd149c929574af5c16eda1e5e512cea5
Elvislml <60972767+Elvislml@users.noreply.github.com>
2020-12-10 13:05:28 -0500

    Crud_Jquery

--- Crud_Jquery/Libreria/crud.js
+++ Crud_Jquery/Libreria/crud.js
@@ -42,19 +42,47 @@ $(document).ready(function(){
         });
     });
 
-    $(document).on('click','.task-item',(e)=>{
-        const elemento = $(this)[0].activeElement.parentElement.parentElement;
-        const id = $(elemento).attr('taskId');
-        console.log(id);
+    $(document).on('click', '.task-item', (e)=>{
+		const elemento = $(this)[0].activeElement.parentElement.parentElement;
+		const id = $(elemento).attr('taskId');
+		console.log(id);
+		$.post('getTareas.php',{id},(response)=>{
+			console.log(response);
+			const task = JSON.parse(response);
+			$('#name').val(task.name);
+			$('#description').val(task.description);
+			$('#taskId').val(task.id);
+			modificar = true;
+		});
+	});
 
-        $.post('getTareas.php',{id},(response) =>{
-            console.log(response);
-            const task = JSON.parse(response);
-            $('#name').val(task.name);
-            $('#description').val(task.description);
-            $('#id').val(task.id);
-            modificar=true;
-        });
+    $(document).on('click', '.task-delete', (e)=>{
+		if(confirm('Desea eliminar el registros')){
+			const elemento = $(this)[0].activeElement.parentElement.parentElement;
+			const id = $(elemento).attr('taskId');
+			$.post('eliminar.php',{id},(response)=>{
+				obtenerTareas();
+			});
+		}
     });
-
+    
+    $('#search').keyup(function(){
+		if($('#search').val()){
+			let search = $('#search').val();
+			$.ajax({
+				url: 'buscar.php',
+				type: 'POST',
+				data: {search},
+				success: function(response){
+					let tasks = JSON.parse(response);
+					let template = '';
+					tasks.forEach(task =>{
+                        template += `<li>${task.name}</li>
+                        <li>${task.description}</li>`;
+					});
+					$('#container').html(template);
+				}
+			});
+		}
+	});
 });
