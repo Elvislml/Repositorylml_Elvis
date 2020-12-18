27f822a5631d4997d1f40359fe41aeb8a74990f9
Elvislml <60972767+Elvislml@users.noreply.github.com>
2020-12-18 13:42:39 -0500

    Django

--- Django/djangoProyect/apps/gestion_clientes/urls.py
+++ Django/djangoProyect/apps/gestion_clientes/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "cliente"),
-    path('crearClientes', views.index, name = "crearClientes"),
+    path('crearClientes', views.crearCliente, name = "crearClientes"),
 ]
--- Django/djangoProyect/apps/gestion_clientes/views.py
+++ Django/djangoProyect/apps/gestion_clientes/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from apps.modelo.models import Cliente
+from .forms import FormularioCliente,FormularioCuenta
 # Create your views here.
 
 def index(request):
-    return render(request,"clientes/index.html")
+    #manejo de ORM
+    listaClientes = Cliente.objects.all()
+    return render(request,"clientes/index.html", locals())
 
 def crearCliente(request):
-    return render(request,"clientes/crear.html")
+    formCliente = FormularioCliente(request.POST)
+    if request.method == 'POST':
+        return redirect(index)
+    return render(request,"clientes/crear.html", locals())
 
 def crearCuenta(request):
     return render(request,"Hola crear")
